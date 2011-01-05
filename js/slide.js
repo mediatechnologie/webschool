f70fd742a5d0ceb513a5a6dfe5372a9458cc659e
duckson <mathijs.bernson@gmail.com>
2011-01-05 12:25:32 +0000

    Tables can now be sorted thanks to the jQuery tablesorter. Various javascript files merged into webschool.js

--- js/slide.js
+++
@@ -1,7 +0,0 @@
-$("#headertoggle").click(function () {
-	$("header #logo").slideToggle("slow");
-});
-
-$(".box h3").click(function () {
-	$(this).slideToggle("slow");
-});
--- js/webschool.js
+++ js/webschool.js
@@ -1,1 +1,22 @@
 // General webkit javascript
+
+$("header h1").mouseover(function()
+{
+	var vlak = 'header';
+	var nummer = Math.floor(Math.random()*6) + 1;
+	var kleuren = new Array("#330066", "#660000", "#006600", "#006666", "#666600", "#663300", "#560066");
+
+	$(vlak).css("background-color", kleuren[nummer]);
+});
+
+$("#headertoggle").click(function () {
+	$("header #logo").slideToggle("slow");
+});
+
+$(".box h3").click(function () {
+	$(this).slideToggle("slow");
+});
+
+$(document).ready(function() { 
+	$(".box table").tablesorter(); 
+}); 
