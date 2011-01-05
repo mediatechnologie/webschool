61a77a525c4299ccef2e32bc873ddff96308136b
duckson <mathijs.bernson@gmail.com>
2011-01-05 12:25:32 +0000

    Tables can now be sorted thanks to the jQuery tablesorter. Various javascript files merged into webschool.js

--- js/kleuren.js
+++
@@ -1,8 +0,0 @@
-$("header h1").mouseover(function()
-{
-	var vlak = 'header';
-	var nummer = Math.floor(Math.random()*6) + 1;
-	var kleuren = new Array("#330066", "#660000", "#006600", "#006666", "#666600", "#663300", "#560066");
-
-	$(vlak).css("background-color", kleuren[nummer]);
-});
