78720a5a15a8696ff65d8460ad1ade42091a601b
duckson <mathijs.bernson@gmail.com>
2011-01-06 08:01:25 +0000

    Moved the code for generating the menu to its own method within the webschool class.

--- classes/webschool.class.php
+++ classes/webschool.class.php
@@ -25,31 +25,10 @@ class webschool
 	{
 		$tags = array(
 			'name' 			=> 'Webschool',
-			'schoolname'		=> 'Mediacollege Amsterdam',
-			'url'			=> 'http://localhost/webschool/'
+			'schoolname'	=> 'Mediacollege Amsterdam',
+			'url'			=> 'http://localhost/webschool/',
+			'menu'			=> $this->menu()
 		);
-
-		$menu = array(
-			'Homepage'		=>  'dashboard',
-			'Roosters'		=>  'schedule',
-			'Leerlingen'	=>  'students',
-			'Agenda'		=>  'agenda',
-			'Webmail'		=>  'mail'
-		);
-
-		// Create menu HTML code
-	    foreach ($menu as $page => $link)
-	    {
-			$parameters = '';
-			if($this->type == $link)
-			{
-			    $parameters = 'class="active"';
-			}
-			
-			$menu_content .= '<li><a '.$parameters.' href="?type='.$link.'">'.$page.'</a></li>';
-	    }
-
-		$tags['menu'] = '<ul>'.$menu_content.'</ul>';
 
 		try
 		{
@@ -74,6 +53,36 @@ class webschool
 		$layout = new template('html/webschool.html', $tags);
 		$layout->parse();
 		return $layout->output();
+	}
+	
+	/**
+	 * Generate the menu, output an unordered list
+	 * @access protected
+	 * @return void
+	 */
+	protected function menu()
+	{
+		$menu = array(
+			'Homepage'		=>  'dashboard',
+			'Roosters'		=>  'schedule',
+			'Leerlingen'	=>  'students',
+			'Agenda'		=>  'calendar',
+			'Webmail'		=>  'mail'
+		);
+
+		// Create menu HTML code
+	    foreach ($menu as $page => $link)
+	    {
+			$parameters = '';
+			if($this->type == $link)
+			{
+			    $parameters = 'class="active"';
+			}
+			
+			$menu_content .= '<li><a '.$parameters.' href="?type='.$link.'">'.$page.'</a></li>';
+	    }
+
+		return '<ul>'.$menu_content.'</ul>';	
 	}
 }
 ?>
