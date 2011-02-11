076ad5c9d3447ffec9fa37d127d8f6e03fef5f3c
Danny <danny@kriger.nl>
2011-02-11 05:04:26 -0800

    Inloggen op WebSchool.

--- classes/user.class.php
+++ classes/user.class.php
@@ -6,23 +6,73 @@
  */
 class user extends database
 {
-	protected $ses = $_SESSION;
+	protected $ses;
 	public $username;
 	private $password;
 	
-	function _init()
+	public function __construct()
 	{
-		if(!empty($this->ses['username']))
+		parent::__construct();
+		
+		if(empty($_SESSION['username']))
+		{
+			$this->verify($_POST['username'], $_POST['password']);
+		}
+		
+		if ($_GET['error'] == 'login')
+		{
+			// Tijdelijke error voor als de gebruikersnaam of wachtwoord niet correct is.
+			echo '
+			<div id="error">
+				Jouw gebruikersnaam of wachtwoord is niet correct.
+			</div>			
+			';
+		}
+	}
+	
+	private function verify($username, $password)
+	{
+		if($_POST['do'] == 'login')
+		{
+			// Username ophalen en beveiligen
+			$username = $_POST['username'];
+				$username = stripslashes($username);
+			// Password ophalen en beveiligen
+			$password = md5($_POST['password']);
+				$password = stripslashes($password);
+				
+			// SQL uitvoeren om te controleren of een gebruiker bestaat.
+			$sql = "SELECT * FROM `user` WHERE `username` = '$username' AND `password` = '$password'";
+			$stmt = $this->db->query($sql);
+			$result = $stmt->rowCount();
+
+			if($result == 1)
+			{
+				// Indien de gebruiker de goede gegevens heeft ingevuld dan worden de gegevens als sessie opgeslagen.
+				$result = $stmt->fetch();
+				$_SESSION['username'] = $result['username'];
+				$_SESSION['firstname'] = $result['firstname'];
+				$_SESSION['lastname'] = $result['lastname'];
+				header('location: index.php');
+			}
+			elseif($result == 0)
+			{
+				// Indien de gebruiker verkeerde gegevens heeft ingevoerd word hij/zij naar deze pagina doorgestuurd.
+				header('location: index.php?error=login');
+			}
+		}
 	}
 	
 	function login()
 	{
+		// Start session
 		session_start();
 		$_SESSION['user_id'] = $this->user_id;
 	}
 	
 	function logout()
 	{
+		// Loguit dmv Session Destroy.
 		session_destroy();
 	}
 }
