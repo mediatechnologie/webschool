d35bd44486973741bddf15c2745e923d57a788e3
Danny <danny@kriger.nl>
2011-02-14 10:43:03 -0800

    De mail class is een stuk beter gemaakt door Farid vandaag.

--- modules/mail.class.php
+++ modules/mail.class.php
@@ -1,16 +1,60 @@
 <?php
-	class mail extends database implements page
+class Mail extends database implements page
+{
+	private $title = 'Webmail';
+	private $count_unread;
+	private $count_total;
+
+	public function __construct()
 	{
-		public $title = 'Webmail';
-		
-		function title()
+		parent::__construct();
+	}
+	
+	public function title()
+	{
+		return $this->title;
+	}
+
+	public function content()
+	{
+		$messages = $this->fetchMail();
+		foreach($messages as $row => $key)
 		{
-			return $this->title;
-		}
-		
-		function content()
-		{
-			// Dummy content
+
+			// Add to Array which should be changed to names & classes
+			$id_list[] = $key['from'];
+
+			// Make Date
+			$date = explode(' ', $key['date']);
+			$date = explode('-', $date[0]);
+
+			// Mark the unread messages
+			if($key['read'] == 0)
+			{
+				$attributes = 'class="message-unread"';
+				$unread++;
+			}
+
+			// Names & Classes
+			$from = $key['from'];
+			$class = $key['from'];
+
+			$mail_list.='
+				<tr '.$attributes.'>
+					<td>
+					<input name="check" type="checkbox" value="L04572">
+					</td>
+					<td>'.$from.' ('.$class.')</td>
+					<td><img src="img/icons/email_open.png"/></td>
+					<td>'.$key['subject'].'</td>
+					<td>'.$date[2].'/'.$date[1].'/'.$date[0].'</td>
+					<td>
+					<img src="img/icons/add.png">
+					</td>
+				</tr>
+			';
+
+
 			$content = '<table>
 			<thead>
 				<tr>
@@ -23,69 +67,49 @@
 				</tr>
 			</thead>
 			<tbody>
-				<tr class="message-unread">
-					<td>
-					<input name="check" type="checkbox" value="L04572">
-					</td>
-					<td>Danny Kriger (MT2A)</td>
-					<td><img src="img/icons/email.png"/></td>
-					<td>Begin Webschool</td>
-					<td>05/01/2011</td>
-					<td>
-					<img src="img/icons/add.png">
-					</td>
-				</tr>
-				<tr>
-					<td>
-					<input name="check" type="checkbox" value="L04572">
-					</td>
-					<td>Danny Kriger (MT2A)</td>
-					<td><img src="img/icons/email_open.png"/></td>
-					<td>Begin Webschool</td>
-					<td>05/01/2011</td>
-					<td>
-					<img src="img/icons/add.png">
-					</td>
-				</tr>
-				<tr>
-					<td>
-					<input name="check" type="checkbox" value="L04572">
-					</td>
-					<td>Danny Kriger (MT2A)</td>
-					<td><img src="img/icons/email_open.png"/></td>
-					<td>Begin Webschool</td>
-					<td>05/01/2011</td>
-					<td>
-					<img src="img/icons/add.png">
-					</td>
-				</tr>
+			'.$mail_list.'
 			</tbody>
 			</table>
 			<p>
 			<input name="send" type="button" value="Formulier verzenden">
 			</p>';
-			$Inbox = new box('Inbox', $content);
-			
+
+		}
+
+			$id_list = array_unique($id_list);
+			//print_r($id_list);
+			$Inbox = new Box('Messages', $content);
 			return $Inbox->getBox();
-		}
-		
-		function boxes()
-		{
-			// Some more dummy content
-			$content = '<form method="post"><p>Naam</p><form method="post"><p><input type="text" size="32" name="search" /> <input type="submit" value="Zoeken" /></p></form>';
-			$hoi = new box('Webschool', $content);
-			
-			return $hoi->getBox();
-		}
-		
-		function warnings()
-		{
-		    /*
-			$waarschuwing = new box('', 'Hier komt Webmail!');
-			
-			return $waarschuwing->getWarning('error');
-		     *
-		     */
-		}
 	}
+
+	public function boxes()
+	{
+		// Inbox
+		$content = '
+			<ul>
+				<li>Nieuw bericht</li>
+				<li>Postvak IN</li>
+				<li>Verstuurd</li>
+				<li>Verwijderd</li>
+			</ul>
+		';
+		$Inbox = new Box('Inbox', $content);
+
+		return $Inbox->getBox();
+	}
+
+	public function warnings()
+	{
+	}
+
+	private function fetchMail($start = 0)
+	{
+		// Make SQL query
+		$sql = "SELECT * FROM `message` WHERE `to` = '".$_SESSION['userid']."' AND `parent` = '0' LIMIT $start, 25";
+
+		$messages = $this->db->query($sql);
+
+		return $messages->fetchAll();
+	}
+}
 ?>
